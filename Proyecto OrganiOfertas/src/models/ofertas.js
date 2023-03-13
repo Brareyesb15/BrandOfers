8921498a463c8ea568b5afca484c0ebfc7ced57d
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-03-12 20:51:06 -0500

    Creación primera tabla ofertas

---
+++ Proyecto OrganiOfertas/src/models/ofertas.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize'); 
+
+module.exports = sequelize => {
+    sequelize.define("ofertas", {
+        empresa: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        descripcion: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        tecnologias:{
+            type: DataTypes.ARRAY(DataTypes.STRING), //
+            allowNull: true
+        },
+        stack: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        active : {
+            type: DataTypes.BOOLEAN,   
+            defaultValue : true, 
+        },
+        linkOferta : {
+            type: DataTypes.INTEGER,
+            allowNull:true
+        },
+        fechaPresentacion: {
+            type: DataTypes.DATEONLY,
+            defaultValue: DataTypes.NOW
+        }
+        
+    },
+        { timestamps: false }
+    );
+};
