e2c7cb40e9cabacaabed82c4a18999d5827efdd5
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-03-18 15:45:09 -0500

    modelo pais

---
+++ Back/src/models/idiomas.js
@@ -0,0 +1,11 @@
+// ingles: {
+//     type: DataTypes.ENUM(
+//         "A2",
+//         "A1",
+//         "B2",
+//         "B1",
+//         "C2",
+//         "C1",
+//         "Nativo"
+//     )
+// }
