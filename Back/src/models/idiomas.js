f0f555aa14983fb963f8356eee53d2699648cd27
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-03-19 23:51:09 -0500

    idiomas agregados

--- Back/src/models/idiomas.js
+++ Back/src/models/idiomas.js
@@ -4,10 +4,21 @@ module.exports = sequelize => {
     sequelize.define("idiomas", {
         idio: {
             type: DataTypes.ENUM(
-                "Español",
-                "Inglés",
-                "Coreano",
-                "Aleman") // en el form poner : agregar un nuevo idioma. Lo que tiene que hacer es crear una nueva opción aquí, buscar como en mi amiga.
+                "español",
+                "inglés", 
+                "japonés", 
+                "francés", 
+                "alemán",
+                "ruso", 
+                "mandarín",
+                 "portugués", 
+                 "italiano", 
+                 "polaco", 
+                 "holandés",
+                  "coreano", 
+                  "húngaro", 
+                  "noruego", 
+                  "sueco") // en el form poner : agregar un nuevo idioma. Lo que tiene que hacer es crear una nueva opción aquí, buscar como en mi amiga.
         },
         nivel:{
             type: DataTypes.ENUM(
