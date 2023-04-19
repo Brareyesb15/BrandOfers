7eed965e2aa0ac1875afc1d6411e3fe259442d26
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-18 22:15:31 -0500

    solving duplicated problem

--- Front/organiofertas/src/app/components/SelectorTec.jsx
+++ Front/organiofertas/src/app/components/SelectorTec.jsx
@@ -1,18 +1,30 @@
-
 "use client"
 import React, { useState } from 'react';
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
-export default function SelectorTec({offers, setRoffers, id}) {
+export default function SelectorTec({ offers, setRoffers, id }) {
   const [selectedOptions, setSelectedOptions] = useState([]);
+  console.log(selectedOptions)
+
   const render = ["porte", "pepe", "camino"];
 
-  const handleOptionSelect = (option) => {
-    if (selectedOptions.includes(option)) {
-      setSelectedOptions(selectedOptions.filter((item) => item !== option));
-    } else {
-      setSelectedOptions([...selectedOptions, option]);
-    }
+  const handleOptionSelect = (event) => {
+    const option = event.target.value;
+    
+    setSelectedOptions(prevOptions => {
+      const index = prevOptions.indexOf(option);
+      let newOptions = [...prevOptions];
+    
+      if (index > -1) {
+        // Si la opción ya existe en la lista, la eliminamos
+        newOptions.splice(index, 1);
+      } else {
+        // Si la opción no existe en la lista, la agregamos
+        newOptions.push(option);
+      }
+  
+      return newOptions;
+    });
   };
 
   return (
@@ -24,7 +36,8 @@ export default function SelectorTec({offers, setRoffers, id}) {
           id="select"
           multiple
           value={selectedOptions}
-          onChange={(event) => handleOptionSelect(event.target.value)}
+          onChange={handleOptionSelect}
+          renderValue={(selected) => selected.join(', ')}
         >
           {render.map((option) => (
             <MenuItem key={option} value={option}>
@@ -32,6 +45,11 @@ export default function SelectorTec({offers, setRoffers, id}) {
             </MenuItem>
           ))}
         </Select>
+        <div>
+          {selectedOptions.map(option => (
+            <span key={option}>{option} </span>
+          ))}
+        </div>
       </FormControl>
     </>
   );
