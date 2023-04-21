44c60601e1b33448da58ca55bf203e5200f7146b
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-20 22:12:13 -0500

    funcion traer detalles desde el back

--- Front/organiofertas/src/app/Detalles/[id]/page.jsx
+++ Front/organiofertas/src/app/Detalles/[id]/page.jsx
@@ -2,9 +2,14 @@
 import { usePathname, useSearchParams} from 'next/navigation'
 import React from "react";
 
+const traerDetalles= async (opcion) => {
+    const response =  await fetch(`http://localhost:5000/opciones/${opcion}`)
+    return response.json()
+  }
+
 export default function DetalleID() {
-    let id = parseInt(usePathname().substring(10)) // en next utilizas usePathname para obtener la url.
-    console.log(id)
+    
+    let id = parseInt(usePathname().substring(10)) // en next utilizas usePathname para obtener la url. Aqui le saco el numero a una url que es /Detalles/12
 
 
 
