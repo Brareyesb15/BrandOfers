390f2677026b11800b2059035dc1cb6da24c88e9
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-20 22:10:46 -0500

    seleccionando el ID

--- Front/organiofertas/src/app/Detalles/[id]/page.jsx
+++ Front/organiofertas/src/app/Detalles/[id]/page.jsx
@@ -1,8 +1,16 @@
+"use client"
+import { usePathname, useSearchParams} from 'next/navigation'
+import React from "react";
+
 export default function DetalleID() {
+    let id = parseInt(usePathname().substring(10)) // en next utilizas usePathname para obtener la url.
+    console.log(id)
+
+
+
     return (
       <>
       <div>Estamos en detalle ID</div>
-
       </>
     )
   }
