f57bffe12e5c4d3afc2e36eecebe3d625f1395ac
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-24 22:22:26 -0500

    llamando al back

--- Front/organiofertas/src/app/components/estadisticas/paises/paises.jsx
+++ Front/organiofertas/src/app/components/estadisticas/paises/paises.jsx
@@ -1,9 +1,16 @@
 "use client"
 import Pais from "./chart"
 
+const traerPaises = async () => {
+    const response =  await fetch(`http://localhost:5000/paises`)
+    return response.json()
+  }
 
+const dataProm = traerPaises()
 
 export default function Paises() {
+    
+    const data = use(dataProm)
     
     return (
     <>
