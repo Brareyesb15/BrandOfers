8dab07791eaaaf6199374dd293f7ad6c34403ae3
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-24 21:33:50 -0500

    andando el chart de Tecnologías

--- Front/organiofertas/src/app/components/estadisticas/chart.js
+++ Front/organiofertas/src/app/components/estadisticas/chart.js
@@ -1,10 +1,10 @@
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
     CategoryScale,
     LinearScale,
     PointElement,
-    LineElement,
+    BarElement,
     Title,
     Tooltip,
     Legend,
@@ -15,48 +15,50 @@ ChartJS.register(
     CategoryScale,
     LinearScale,
     PointElement,
-    LineElement,
+    BarElement,
     Title,
     Tooltip,
     Legend,
     Filler
 );
 
-var beneficios = [0, 56, 20, 36, 80, 40, 30, -20, 25, 30, 12, 60];
-var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-var midata = {
-    labels: meses,
-    datasets: [ // Cada una de las líneas del gráfico
-        {
-            label: 'Beneficios',
-            data: beneficios,
-            tension: 0.5,
-            fill : true,
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            pointRadius: 5,
-            pointBorderColor: 'rgba(255, 99, 132)',
-            pointBackgroundColor: 'rgba(255, 99, 132)',
-        },
-        {
-            label: 'Otra línea',
-            data: [20, 25, 60, 65, 45, 10, 0, 25, 35, 7, 20, 25]
-        },
-    ],
-};
+export default function Bars({datos}) {
+// var beneficios = [72, 56, 20, 36, 80, 40, 30, -20, 25, 30, 12, 60];
+// var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+let meses = datos.map(array => array[0]);
+let beneficios = datos.map(array => array[1]);
+
 
 var misoptions = {
+    responsive : true,
+    animation : false,
+    plugins : {
+        legend : {
+            display : false
+        }
+    },
     scales : {
         y : {
-            min : 0
+            min : 0,
+            max : 10
         },
         x: {
-            ticks: { color: 'rgb(255, 99, 132)'}
+            ticks: { color: 'rgba(0, 220, 195)'}
         }
     }
 };
 
-export default function LinesChart({datos}) {
-    return <Line data={midata} options={misoptions}/>
+var midata = {
+    labels: meses,
+    datasets: [
+        {
+            label: 'Beneficios',
+            data: beneficios,
+            backgroundColor: 'rgba(0, 220, 195, 0.5)'
+        }
+    ]
+};
+    return <Bar data={midata} options={misoptions} />
 }
