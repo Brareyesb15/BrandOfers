987432944956c5d315b10a06091d742ac490186e
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-24 23:31:25 -0500

    configurado jsx de postulación

---
+++ Front/organiofertas/src/app/components/estadisticas/postulacion/postulacion.jsx
@@ -0,0 +1,36 @@
+"use client"
+import Postulacion from "./chart"
+import React, { useState, useEffect, use } from 'react';
+
+
+const traerPost = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/post`)
+    return response.json()
+  }
+
+const dataProm = traerPost()
+
+export default function Post() {
+    
+    const data = use(dataProm)
+
+
+    function getPostulaciones(post) {
+        const uniquePost = [...new Set(post)];
+        const counts = uniquePost.map(posto=> post.filter(c => c === posto).length);
+        return [uniquePost, counts];
+      }
+
+        const [uniquePost, counts] = getPostulaciones(data);
+        
+
+    return (
+    <>
+    <div>
+        <Postulacion post={uniquePost} num={counts}></Postulacion>
+   </div>
+    </>
+)
+
+
+}
