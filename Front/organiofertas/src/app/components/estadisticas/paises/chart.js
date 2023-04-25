07e7a46ed4a4fec4e4856ed944bb35e25c9eac24
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-24 23:02:47 -0500

    Terminado paises

--- Front/organiofertas/src/app/components/estadisticas/paises/chart.js
+++ Front/organiofertas/src/app/components/estadisticas/paises/chart.js
@@ -4,36 +4,35 @@ import { Pie } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-var options = {
-    responsive : true,
-    maintainAspectRatio: false,
-};
 
-var data = {
-    labels: ['Carne', 'Jamón', 'Dulces', 'Turrón', 'Vino'],
-    datasets: [
-        {
-            label: 'Popularidad en Navidad',
-            data: [35, 20, 20, 15, 10],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-            ],
-            borderWidth: 1,
-        },
-    ],
-};
+export default function Pais({paises,num}) {
 
-export default function Pais() {
+    let options = {
+        responsive : true,
+        maintainAspectRatio: false,
+    };
+    
+    let data = {
+        labels: paises,
+        datasets: [
+            {
+                label: "Numero de ofertas por país",
+                data: num,
+                backgroundColor: [
+                    '#641E16 ',
+                    '#17202A',
+                    '#7B7D7D    ',
+                    '#145A32',
+                ],
+                borderColor: [
+                    '#641E16 ',
+                    '#17202A',
+                    '#7B7D7D    ',
+                    '#145A32',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
     return <Pie data={data} options={options} />
 }
--- Front/organiofertas/src/app/components/estadisticas/paises/paises.jsx
+++ Front/organiofertas/src/app/components/estadisticas/paises/paises.jsx
@@ -1,8 +1,9 @@
 "use client"
 import Pais from "./chart"
+import React, { useState, useEffect, use } from 'react';
 
 const traerPaises = async () => {
-    const response =  await fetch(`http://localhost:5000/paises`)
+    const response =  await fetch(`http://localhost:5000/stats/paises`)
     return response.json()
   }
 
@@ -10,12 +11,23 @@ const dataProm = traerPaises()
 
 export default function Paises() {
     
-    // const data = use(dataProm)
+    const data = use(dataProm)
+    console.log(data)
+
+    function getCountryCounts(countries) {
+        const uniqueCountries = [...new Set(countries)];
+        const counts = uniqueCountries.map(country => countries.filter(c => c === country).length);
+        return [uniqueCountries, counts];
+      }
+
+        const [uniqueCountries, counts] = getCountryCounts(data);
+        console.log(uniqueCountries);
+        console.log(counts);
 
     return (
     <>
     <div>
-        <Pais></Pais>
+        <Pais paises={uniqueCountries} num={counts}></Pais>
    </div>
     </>
 )
