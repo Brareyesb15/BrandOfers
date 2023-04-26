fb3971bf21129177c2b837caedfcd3a124b40685
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-25 23:27:36 -0500

    Funcion andando (controlador) perfecto

--- Back/src/controllers/putActivate.js
+++ Back/src/controllers/putActivate.js
@@ -5,7 +5,12 @@ const putActivate = async (req,res) => {
     let {id} = req.params
     console.log("entreeee", id)
     try{
-        
+        const oferta = await ofertas.findOne({ where: { id } }); 
+    
+        const newActivate = !oferta.active;
+     
+        await oferta.update({ active: newActivate });
+        const result = await ofertas.findAll()
     
   return res.status(200).send(result)
     
