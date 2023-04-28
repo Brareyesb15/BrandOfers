bdbd57c65c9bb51883a0781a55f344bf2def20bc
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-04-27 22:47:06 -0500

    editar componente avances

--- Front/organiofertas/src/app/components/avances.jsx
+++ Front/organiofertas/src/app/components/avances.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState } from "react"
 
-export default function NewComponent () {
+export default function Avances () {
     const [mostrar,setMostrar] = useState(false)
 
 
