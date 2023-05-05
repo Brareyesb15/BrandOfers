041481945db8b0f113a39953c2a32de3b6afeecb
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-04 20:56:02 -0500

    creado componente de plats

---
+++ Front/organiofertas/src/app/components/estadisticas/plataformas/plats.jsx
@@ -0,0 +1,42 @@
+"use client"
+import React, { useState, useEffect, use } from 'react';
+import LinesChart from './chart';
+
+
+const traerPlats = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/plataformas`)
+    return response.json()
+  }
+
+const dataProm = traerPlats()
+
+export default function Plats() {
+  
+  function plataformasMasComunes(matriz) {
+    const frecuenciaInversa = {};
+    for (const lista of matriz) {
+      const platsUnicas = new Set(lista);
+      for (const plats of platsUnicas) {
+        if (plats in frecuenciaInversa) {
+          frecuenciaInversa[plats] += 1;
+        } else {
+          frecuenciaInversa[plats] = 1;
+        }
+      }
+    }
+    const plataformasOrdenadas = Object.entries(frecuenciaInversa).sort((a, b) => b[1] - a[1]).slice(0, 15);
+    return plataformasOrdenadas.map(([plataforma, frecuencia]) => [plataforma, frecuencia]);
+  } 
+
+      const platsMas = plataformasMasComunes(matrix)
+     
+    return (
+        <>
+      <div>
+        <LinesChart datos={platsMas}></LinesChart>
+      </div>
+        </>
+    )
+
+
+  }
