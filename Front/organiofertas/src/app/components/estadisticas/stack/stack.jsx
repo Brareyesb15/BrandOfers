801e658cdafe8e672b6b28604db6fa0c8dd7f963
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-12 00:15:31 -0500

    config front de stack

--- Front/organiofertas/src/app/components/estadisticas/stack/stack.jsx
+++ Front/organiofertas/src/app/components/estadisticas/stack/stack.jsx
@@ -4,15 +4,27 @@ import React, { use } from 'react';
 
 
 const traerStack = async () => {
-    const response =  await fetch(`http://localhost:5000/stats/stack`)
+    const response =  await fetch(`http://localhost:5000/stats/stack/1`)
+    return response.json()
+  }
+const traerStack2 = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/stack/2`)
+    return response.json()
+  }
+const traerStack3 = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/stack/3`)
     return response.json()
   }
 
 const dataProm = traerStack()
+const dataProm2 = traerStack2()
+const dataProm3 = traerStack3()
 
-export default function Stack() {
+
+
+export default function Stack({id}) {
     
-    const data = use(dataProm)
+  const data = id === 1 ? use(dataProm) : id === 2 ? use(dataProm2) : id === 3? use(dataProm3) : null ;
 
 
     function getStack(stack) {
