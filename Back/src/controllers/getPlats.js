8b67734ad691c0a8105e5aa9d7f084310df37160
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-12 00:34:32 -0500

    config back de plataformas y andando componente completo

--- Back/src/controllers/getPlats.js
+++ Back/src/controllers/getPlats.js
@@ -1,22 +1,50 @@
-
-
-const {ofertas} = require("../../configs/DB_connection");
+const {ofertas,avances} = require("../../configs/DB_connection");
 
 
 const getPlats = async (req,res) => {
-    
-    try{
-        const oferta = await ofertas.findAll({
-            attributes: ["plataforma"]
-          })
-        const result = oferta.map(oferta => oferta.plataforma);
-       
-  return res.status(200).send(result)
-    
-}
-    catch(error){
-        return res.status(400).send(error.message)
+    const { id } = req.params;
+
+  try {
+    if (id === "1") {
+      const oferta = await ofertas.findAll({
+        attributes: ["plataforma"],
+      });
+      const result = oferta.map((oferta) => oferta.plataforma);
+      return res.status(200).send(result);
     }
-}
+    if (id === "2") {
+      const oferta = await ofertas.findAll({
+        attributes: ["plataforma"],
+        include: [
+          {
+            model: avances,
+            required: true,
+          },
+        ],
+      });
+      const result = oferta.map((oferta) => oferta.plataforma);
+      return res.status(200).send(result);
+    }
+    if (id === "3") {
+      const oferta = await ofertas.findAll({
+        attributes: ["plataforma"],
+        include: [
+          {
+            model: avances,
+            required: false,
+          },
+        ],
+        where: {
+          "$avances.id$": null,
+        },
+      });
+      const result = oferta.map((oferta) => oferta.plataforma);
+      return res.status(200).send(result);
+    }
+    return res.status(400).send("Invalid ID");
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+};
 
 module.exports = getPlats; 
