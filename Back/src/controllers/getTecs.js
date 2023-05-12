7b976dda0c56ca46436ee6a93ac95ee059bfa6da
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-11 23:43:47 -0500

    andando tecs desde el back

--- Back/src/controllers/getTecs.js
+++ Back/src/controllers/getTecs.js
@@ -1,20 +1,49 @@
-const {ofertas} = require("../../configs/DB_connection");
+const { ofertas, avances } = require("../../configs/DB_connection");
 
+const getTecs = async (req, res) => {
+  const { id } = req.params;
+  console.log(id)
 
-const getTecs = async (req,res) => {
-    
-    try{
-        const oferta = await ofertas.findAll({
-            attributes: ['tecnologias']
-          })
-        const result = oferta.map(oferta => oferta.tecnologias);
-       
-  return res.status(200).send(result)
-    
-}
-    catch(error){
-        return res.status(400).send(error.message)
+  try {
+    if (id === "1") {
+      const oferta = await ofertas.findAll({
+        attributes: ["tecnologias"],
+      });
+      const result = oferta.map((oferta) => oferta.tecnologias);
+      return res.status(200).send(result);
     }
-}
-
+    if (id === "2") {
+      const oferta = await ofertas.findAll({
+        attributes: ["tecnologias"],
+        include: [
+          {
+            model: avances,
+            required: true,
+          },
+        ],
+      });
+      const result = oferta.map((oferta) => oferta.tecnologias);
+      return res.status(200).send(result);
+    }
+    if (id === "3") {
+      const oferta = await ofertas.findAll({
+        attributes: ["tecnologias"],
+        include: [
+          {
+            model: avances,
+            required: false,
+          },
+        ],
+        where: {
+          "$avances.id$": null,
+        },
+      });
+      const result = oferta.map((oferta) => oferta.tecnologias);
+      return res.status(200).send(result);
+    }
+    return res.status(400).send("Invalid ID");
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+};
 module.exports = getTecs; 
