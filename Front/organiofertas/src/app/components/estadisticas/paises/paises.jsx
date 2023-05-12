b5a3b62428bb3de48edecd9f472f42f9403f2fc4
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-11 23:53:03 -0500

    front de paises andando

--- Front/organiofertas/src/app/components/estadisticas/paises/paises.jsx
+++ Front/organiofertas/src/app/components/estadisticas/paises/paises.jsx
@@ -3,16 +3,25 @@ import Pais from "./chart"
 import React, { useState, useEffect, use } from 'react';
 
 const traerPaises = async () => {
-    const response =  await fetch(`http://localhost:5000/stats/paises`)
+    const response =  await fetch(`http://localhost:5000/stats/paises/1`)
+    return response.json()
+  }
+  const traerPaises2 = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/paises/2`)
+    return response.json()
+  }
+  const traerPaises3 = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/paises/3`)
     return response.json()
   }
 
 const dataProm = traerPaises()
+const dataProm2 = traerPaises()
+const dataProm3= traerPaises()
 
 export default function Paises() {
     
-    const data = use(dataProm)
-    console.log(data)
+  const data = id === 1 ? use(dataProm) : id === 2 ? use(dataProm2) : id === 3? use(dataProm3) : null ;
 
     function getCountryCounts(countries) {
         const uniqueCountries = [...new Set(countries)];
