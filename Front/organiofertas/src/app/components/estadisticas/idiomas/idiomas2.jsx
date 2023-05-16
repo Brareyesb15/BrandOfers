069d64c06801615d5290357c7012764ca1490ff0
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-15 23:55:38 -0500

    Configurando front funciones para funcionar idioma2

--- Front/organiofertas/src/app/components/estadisticas/idiomas/idiomas2.jsx
+++ Front/organiofertas/src/app/components/estadisticas/idiomas/idiomas2.jsx
@@ -1,5 +1,6 @@
 "use client"
 import BarChart from "./chart2";
+import React, { use } from 'react';
 
 const traerIdioma = async () => {
     const response =  await fetch(`http://localhost:5000/stats/idioma2/1`)
@@ -20,9 +21,39 @@ const dataProm3 = traerIdioma3()
 
 export default function Idioma2({id}) {
     const data = id === 1 ? use(dataProm) : id === 2 ? use(dataProm2) : id === 3? use(dataProm3) : null ;
-  
+    
+    function obtenerInformacionIdiomas(info) {
+        // Obtener los idiomas únicos y su cantidad de repeticiones
+        const idiomas = {};
+        info.forEach((item) => {
+          const { idioma, nivel } = item;
+          if (!idiomas[idioma]) {
+            idiomas[idioma] = { num: 1, niveles: { [nivel]: 1 } };
+          } else {
+            idiomas[idioma].num++;
+            if (!idiomas[idioma].niveles[nivel]) {
+              idiomas[idioma].niveles[nivel] = 1;
+            } else {
+              idiomas[idioma].niveles[nivel]++;
+            }
+          }
+        });
+      
+        // Generar el nuevo array de objetos
+        const resultado = Object.keys(idiomas).map((idioma) => ({
+          nombre: idioma,
+          num: idiomas[idioma].num,
+          niveles: Object.entries(idiomas[idioma].niveles).map(([nivel, repeticiones]) => ({
+            nivel,
+            repeticiones,
+          })),
+        }));
+      
+        return resultado;
+      }
 
-          
+      const resultado = obtenerInformacionIdiomas(data);
+      console.log("RESULLLLLLLLLLLL", resultado);
   
       return (
       <>
