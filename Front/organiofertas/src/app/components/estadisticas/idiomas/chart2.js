25b723cfad4d7b1cf8d39d98c306a5b15b5f9d40
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-16 00:46:49 -0500

    Andando chart perfecto

--- Front/organiofertas/src/app/components/estadisticas/idiomas/chart2.js
+++ Front/organiofertas/src/app/components/estadisticas/idiomas/chart2.js
@@ -24,24 +24,45 @@ ChartJS.register(
 
 
 export default function BarChart({datos}) {
+    console.log(datos)
+    function getBackgroundColor(nivel) {
+        switch (nivel) {
+          case 'A1':
+            return 'rgba(1, 99, 132, 0.5)';
+          case 'A2':
+            return 'rgba(100, 162, 235, 0.5)';
+          case 'B1':
+            return 'rgba(200, 206, 86, 0.5)';
+          case 'B2':
+            return 'rgba(300, 192, 192, 0.5)';
+          case 'C1':
+            return 'rgba(900, 102, 255, 0.5)';
+          case 'C2':
+            return 'rgba(500, 159, 64, 0.5)';
+          case 'Nativo':
+            return 'rgba(600, 162, 235, 0.5)';
+          default:
+            return 'rgba(0, 0, 0, 0.5)';
+        }
+      }
 
-    const info = datos.map((item) => item.nombre); 
+      const nivelesDisponibles = Array.from(
+        new Set(datos.flatMap(objeto => objeto.niveles.map(nivel => nivel.nivel)))
+      );
+      
+      // Construir la estructura de datos para Chart.js
+      const data = {
+        labels: datos.map(objeto => objeto.nombre), // Nombres de idioma
+        datasets: nivelesDisponibles.map(nivel => ({
+          label: nivel,
+          data: datos.map(objeto => {
+            const nivelEncontrado = objeto.niveles.find(n => n.nivel === nivel);
+            return nivelEncontrado ? nivelEncontrado.repeticiones : 0;
+          }),
+          backgroundColor: getBackgroundColor(nivel), // Función para obtener el color de fondo basado en el nivel
+        })),
+      };    
 
-  const data = {
-    labels: info,
-    datasets: [
-      {
-        label: 'Subcategoría 1',
-        data: [10, 15, 20],
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Subcategoría 2',
-        data: [5, 8, 12],
-        backgroundColor: 'rgba(54, 162, 235, 0.5)',
-      },
-    ],
-  };
 
   const options = {
     scales: {
