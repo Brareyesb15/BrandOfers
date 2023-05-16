01363ce703b9b96c7513b76eb0a877fcef16d982
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-15 23:36:24 -0500

    Config idioma2

--- Front/organiofertas/src/app/components/estadisticas/idiomas/idiomas2.jsx
+++ Front/organiofertas/src/app/components/estadisticas/idiomas/idiomas2.jsx
@@ -1,7 +1,25 @@
 "use client"
 import BarChart from "./chart2";
 
-export default function Idioma2() {
+const traerIdioma = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/idioma/1`)
+    return response.json()
+  }
+  const traerIdioma2 = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/idioma/2`)
+    return response.json()
+  }
+  const traerIdioma3 = async () => {
+    const response =  await fetch(`http://localhost:5000/stats/idioma/3`)
+    return response.json()
+  }
+
+const dataProm = traerIdioma()
+const dataProm2 = traerIdioma2()
+const dataProm3 = traerIdioma3()
+
+export default function Idioma2({id}) {
+    const data = id === 1 ? use(dataProm) : id === 2 ? use(dataProm2) : id === 3? use(dataProm3) : null ;
   
 
           
