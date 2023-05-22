299898ae3f9e9cae382333bba15793e08e458377
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-22 16:33:55 -0500

    Texto en welcome

--- Front/organiofertas/src/app/components/welcome.jsx
+++ Front/organiofertas/src/app/components/welcome.jsx
@@ -6,7 +6,10 @@ export default function Welcome() {
     return (
         <div>
             <p >Hola! Esto es Organi Ofertas, una app que te ayudará a organizar tus postulaciones!! 
-                Somos conscientes de que 
+                Somos conscientes de la difícil tarea que es intentar conseguir un trabajo, para ello se hace
+                necesario mantener un orden de todas tus postulaciones, por eso creamos Organi Ofertas. 
+                Con organiOfertas tendrás un control muy cuidado de todas las ofertas a las que postules, veamos como: 
+                
             </p>
             
         </div>
