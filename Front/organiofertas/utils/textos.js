9092930489cf000f60b6046e4e316f3b50db2b76
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-22 16:53:03 -0500

    Modularizando

---
+++ Front/organiofertas/utils/textos.js
@@ -0,0 +1,25 @@
+
+
+const textHome = <div>
+<p>Hola! Esto es Organi Ofertas, una app que te ayudará a organizar tus postulaciones!! 
+    Somos conscientes de la difícil tarea que es intentar conseguir un trabajo, para ello se hace
+    necesario mantener un orden de todas tus postulaciones, por eso creamos Organi Ofertas. 
+    Con organiOfertas tendrás un control muy cuidado de todas las ofertas a las que postules, veamos como: </p>
+
+<p>Home es tu página principal, aquí tendrás la tabla general de ofertas, en ella tendrás en principio todas las ofertas
+    a las que has aspirado, pero podrás seleccionar si decides ver las ofertas que siguen activas(es decir que no han
+    recibido respuesta negativa) o inactivas(con respuesta negativa). </p>
+
+<p>Luego tenemos Guardar Oferta, el espacio donde podrás llenar el formulario de oferta, es decir, si aspiraste a 
+    alguna oferta podrás llenar toda la información sobre ella. </p>
+
+<p>Por último tenemos estadisticas, en estadisticas tendrás información valiosa sobre tus ofertas, lo que te permitirá
+    estudiar a qué estás aspirando, que mecanismos y estrategias te están funcionando mejor y en que puedes mejorar respecto
+    a tus estrategías de busqueda laboral. </p>
+    
+<p>Tenemos más información especifica sobre como usar la app en cada una de las paginas. Por lo pronto es una app muy enfocada al mundo del desarrollo web, pero esperamos a futuro poder ampliarla. 
+    Que la disfrutes!</p>
+
+</div>;
+
+export default textHome;
