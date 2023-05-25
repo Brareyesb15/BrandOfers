642f6aadec9989fbf505d7599a2e6b687f0f98af
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-25 18:21:12 -0500

    agregados iconos al nav bar

--- Front/organiofertas/src/app/components/nav bar.jsx
+++ Front/organiofertas/src/app/components/nav bar.jsx
@@ -1,35 +1,43 @@
-import Link from "next/link"
-import "./nav bar.css"
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faSave, faChartBar } from "@fortawesome/free-solid-svg-icons";
+import "./nav bar.css";
 
-const links = [{
-    label :  "Home",
-    route: "/"            // trabajo de ruteo, las guardas en un link para trabajarlo.
+const links = [
+  {
+    label: "Home",
+    route: "/",
+    icon: faHome,
   },
   {
     label: "Guardar Oferta",
-    route: "/Guardar"
+    route: "/Guardar",
+    icon: faSave,
   },
   {
-    label : "Estadisticas",
-    route: "/Estadisticas"
-  }
-]
-  export default function Navigation() {
-    return (
-      <header>
+    label: "Estadisticas",
+    route: "/Estadisticas",
+    icon: faChartBar,
+  },
+];
+
+export default function Navigation() {
+  return (
+    <header className="header">
       <nav>
         <ul className="navbar-items">
-          {links.map(({ label, route }) => (
+          {links.map(({ label, route, icon }) => (
             <li key={route}>
               <Link href={route}>
-                <div className="navbar-item">{label}</div>
+                <div className="navbar-item">
+                  <FontAwesomeIcon icon={icon} />
+                  {label}
+                </div>
               </Link>
             </li>
           ))}
         </ul>
       </nav>
     </header>
-    
-    )
-  }
- 
+  );
+} 
