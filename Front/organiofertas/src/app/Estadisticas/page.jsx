e4cd84aa1a2dd533bd045e43695ae31a705547ae
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-24 23:49:48 -0500

    Selector alineado

--- Front/organiofertas/src/app/Estadisticas/page.jsx
+++ Front/organiofertas/src/app/Estadisticas/page.jsx
@@ -32,11 +32,11 @@ export default function Estadisticas() {
   return (
     <>
       <Welcome text={textEsta}></Welcome>
-      <div padding-bottom="500px">
-      {selector}
+      <div className="selectorContainer">
+        {selector}
       </div>
-        <div><Tecs id={selectedOption}>Estadisticas sobre Tecnologías</Tecs></div>
-        <div className="gridContainer"> 
+      <div><Tecs id={selectedOption}>Estadisticas sobre Tecnologías</Tecs></div>
+      <div className="gridContainer"> 
         <div className="componentBox"><Paises id={selectedOption}>Estadisticas sobre Países</Paises></div>
         <div className="componentBox"><Exp id={selectedOption}></Exp></div>
         <div className="componentBox"><Post id={selectedOption}></Post></div>
