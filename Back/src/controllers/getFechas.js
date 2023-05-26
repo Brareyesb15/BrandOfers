8a1ae00e29cf1578bf8504c6161e4311931f04ef
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-05-26 17:41:47 -0500

    backend fechas2

--- Back/src/controllers/getFechas.js
+++ Back/src/controllers/getFechas.js
@@ -2,18 +2,16 @@ const {ofertas,avances} = require("../../configs/DB_connection");
 
 
 const getFechas = async (req,res) => {
-    const { id } = req.params;
+  let resolv = {};
 
   try {
-    if (id === "1") {
-      const oferta = await ofertas.findAll({
+    
+      const oferta1 = await ofertas.findAll({
         attributes: ["fechaPresentacion"],
       });
-      const result = oferta.map((oferta) => oferta.fechaPresentacion);
-      return res.status(200).send(result);
-    }
-    if (id === "2") {
-      const oferta = await ofertas.findAll({
+      resolv.uno = oferta1.map((oferta) => oferta.fechaPresentacion);
+      
+      const oferta2 = await ofertas.findAll({
         attributes: ["fechaPresentacion"],
         include: [
           {
@@ -22,11 +20,9 @@ const getFechas = async (req,res) => {
           },
         ],
       });
-      const result = oferta.map((oferta) => oferta.fechaPresentacion);
-      return res.status(200).send(result);
-    }
-    if (id === "3") {
-      const oferta = await ofertas.findAll({
+     resolv.dos = oferta2.map((oferta) => oferta.fechaPresentacion);
+    
+      const oferta3 = await ofertas.findAll({
         attributes: ["fechaPresentacion"],
         include: [
           {
@@ -38,10 +34,9 @@ const getFechas = async (req,res) => {
           "$avances.id$": null,
         },
       });
-      const result = oferta.map((oferta) => oferta.fechaPresentacion);
-      return res.status(200).send(result);
-    }
-    return res.status(400).send({error: "Invalid ID"});
+      resolv.tres = oferta3.map((oferta) => oferta.fechaPresentacion);
+     
+      return res.status(200).send(resolv)
   } catch (error) {
     return res.status(400).send({error: error.message})
   }
